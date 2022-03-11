ffefda92c70a3db9197e139d6e67a7d5f125c8f8
barisyes <barisyesilkayaa@gmail.com>
2022-03-11 11:47:28 +0300

    githuba proje yüklüyorum

---
+++ console programlama/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("isminizi giriniz: ");
+            string isim = Console.ReadLine();
+
+            Console.Write("soy isminizi giriniz: ");
+            string soyisim = Console.ReadLine();
+
+            Console.WriteLine("merhaba " + isim + " " + soyisim);
+            Console.ReadLine();
+
+        }
+    }
+}
