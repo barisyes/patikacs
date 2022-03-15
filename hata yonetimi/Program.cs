41be8078409df35f144682e99e8fd8268d09ec79
barisyes <barisyesilkayaa@gmail.com>
2022-03-15 14:26:08 +0300

    hata yonetimi dosyasi eklendi

---
+++ hata yonetimi/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Sayi gir: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sayi= " + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("hata: " +ex.Message.ToString());
+            }
+            /*finally
+            {
+                Console.WriteLine("islem tamamlandi.");
+            }*/
+            
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-2000000000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş deger girdiniz.");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun degil.");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("deger uygun aralikta degil.");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("islem basari ile tamamlandi.");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
