51b8b141ede85affa6c8c0dfac5e3435144c6495
barisyes <barisyesilkayaa@gmail.com>
2022-03-16 12:24:23 +0300

    5

---
+++ donguler for each/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace donguler_for_each
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //1 den baslayarak console'dan girilen sayıya kadar olan sayıların ortalaması.
+            Console.WriteLine("Sayi giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+
+            while (sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            //a dan z ye kadar tüm harfleri yazdır.
+            char character = 'a';
+            while (character<='z')
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            //Foreach
+            Console.WriteLine("***** Foreach *****");
+            string[] arabalar={"BMW", "Ford", "Toyota", "Nissan"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+        }
+    }
+}
