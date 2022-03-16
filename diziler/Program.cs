d7e291d30319b47c15574966fe860aa4ec747acb
barisyes <barisyesilkayaa@gmail.com>
2022-03-16 14:24:36 +0300

    6

---
+++ diziler/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi tanimlama
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = {"kedi", "kopek", "Kus", "Meymun"};
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //Dizilere deger atama ve erisim
+            renkler[0] = "mavi";
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //Döngülerle dizi kullanımı
+            //Klavyeden girilen n tane sayinin ortalamasini hesaplayan program
+            Console.WriteLine("dizinin eleman sayisini giriniz.:");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayisini giriniz: ", i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+                int toplam=0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam+=sayi;
+            }
+            Console.WriteLine("Ortalama : " + toplam/diziUzunlugu);
+
+        }
+    }
+}
