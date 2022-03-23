3ecdb69905cd906e5b5a573705292b8ac94431cd
barisyes <barisyesilkayaa@gmail.com>
2022-03-23 09:38:56 +0300

    odev1>1 projesi eklendi.

---
+++ odev1/1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Eleman sayisi giriniz: ");
+            int elemanSayisi = Convert.ToInt32(Console.ReadLine());
+
+            int[] dizi = new int[elemanSayisi];
+
+            for (int i = 0; i < elemanSayisi; i++)
+            {
+                Console.Write("{0}. elemani giriniz: ", i+1);                
+                dizi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < elemanSayisi; i++)
+            {
+                if(dizi[i]%2==0)
+                    Console.Write(dizi[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
