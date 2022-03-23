330f514e25f8005e87529dd6a94afcfc2690804b
barisyes <barisyesilkayaa@gmail.com>
2022-03-23 10:37:28 +0300

    odev1>4 projesi eklendi.

---
+++ odev1/4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cumle giriniz: ");
+            string cumle = Console.ReadLine();
+            string[] dizi = cumle.Split(" ");
+            int kelimeSayisi = dizi.Length;
+            Console.WriteLine("Kelime sayisi: {0}", kelimeSayisi);
+
+            int harfSayisi=0;
+
+            for (int i = 0; i < kelimeSayisi; i++)
+            {
+                harfSayisi += dizi[i].Length;
+            }
+
+            Console.WriteLine("Harf sayisi: {0}", harfSayisi);
+
+            Console.ReadLine();
+        }
+    }
+}
