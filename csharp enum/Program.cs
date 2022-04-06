4705ec1bf3eaee1d3c625dc5429a3b607b2a2ec2
barisyes <barisyesilkayaa@gmail.com>
2022-04-06 13:21:24 +0300

    struct kavrami ve csharp enum dosyalari eklendi.

---
+++ csharp enum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 32;
+
+            if(sicaklik<=(int)HavaDurumu.Soguk)
+                Console.WriteLine("Hava cok soguk.");
+            else if(sicaklik <= (int)HavaDurumu.Normal && sicaklik>(int)HavaDurumu.Soguk)
+                Console.WriteLine("Disariya cikmak icin havanin biraz daha isinmasini bekleyelim.");
+            else if(sicaklik > (int)HavaDurumu.Normal && sicaklik<=(int)HavaDurumu.Sicak)
+                Console.WriteLine("Hadi disari cikalim.");
+            else if(sicaklik>=(int)HavaDurumu.Sicak && sicaklik<(int)HavaDurumu.CokSicak)
+                Console.WriteLine("Disariya cikmak icin cok sicak.");
+            else if(sicaklik>=(int)HavaDurumu.CokSicak)
+                Console.WriteLine("Hava cok sicak.");
+
+
+            Console.ReadLine();
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi=1,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk=5,
+        Normal=20,
+        Sicak=25,
+        CokSicak=30
+    }
+}
---
+++ struct kavrami/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace struct_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diktortgen yenidikdortgen = new Diktortgen();
+            // yenidikdortgen.KisaKenar = 3;
+            // yenidikdortgen.UzunKenar = 4;
+            Console.WriteLine("Class alan hesabi: {0}", yenidikdortgen.AlanHesapla());
+
+            Dikdortgen_Struct dikdortgenStruct = new Dikdortgen_Struct(3,4);
+            Console.WriteLine("Struct alan hesabi: {0}", dikdortgenStruct.AlanHesapla());
+
+            Console.ReadLine();
+        }
+    }
+    class Diktortgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+        public Diktortgen()
+        {
+            KisaKenar=3;
+            UzunKenar=4;
+        }
+        public long AlanHesapla()
+        {
+            return KisaKenar*UzunKenar;
+        }
+    }
+
+    struct Dikdortgen_Struct
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+        public Dikdortgen_Struct(int kisaKenar, int uzunKenar)
+        {
+            KisaKenar = kisaKenar;
+            UzunKenar = uzunKenar;
+        }
+        public long AlanHesapla()
+        {
+            return this.KisaKenar*this.UzunKenar;
+        }
+    }
+}
